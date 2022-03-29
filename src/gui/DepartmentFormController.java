bb95f0e24fa523273d07344467899fc288638236
Pedro Quintanilha <pedrosquinta@gmail.com>
2022-03-29 02:52:11 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -69,8 +72,19 @@ public class DepartmentFormController implements Initializable {
 		    notifyDataChangeListeners();		  
 		    Utils.currentStage(event).close();
 		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 		catch (DbException e) {
 			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private void setErrorMessages(Map<String,String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
 		}
 	}
 
@@ -94,7 +108,7 @@ public class DepartmentFormController implements Initializable {
 
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 30); 
 	}
 
 	public void updateFormData() {
@@ -106,9 +120,24 @@ public class DepartmentFormController implements Initializable {
 	}
 
 	private Department getFormData() {
+		
 		Department obj = new Department();
+		
+		ValidationException exception = new ValidationException("Validation error");
+	
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals(" ")) {
+			exception.addError("name", " Field can't be empty");
+		}
+					
 		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() >0) {
+			throw exception;
+		}
+		
 		return obj;
 	}
+
 }
